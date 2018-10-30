9d7da6550ec3492db72dee416e04c5408f493ef0
KikoLabiano <flabiano@hotmail.com>
2018-10-30 23:55:48 +0100

    Cambios

--- js/trivia-api.js
+++ js/trivia-api.js
@@ -8,8 +8,30 @@ var triviaAPI= (function(){
             .then((response)=>{              
                 return response.json(); 
             });
+        },
+        shuffleAnswers: (answers)=>{
+            return shuffle(answers);
         }
     };
+
+    function shuffle(array) {
+        let currentIndex = array.length, temporaryValue, randomIndex;
+        console.log(array);
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+      
+          // Pick a remaining element...
+          randomIndex = Math.floor(Math.random() * currentIndex);
+          currentIndex -= 1;
+      
+          // And swap it with the current element.
+          temporaryValue = array[currentIndex];
+          array[currentIndex] = array[randomIndex];
+          array[randomIndex] = temporaryValue;
+        }
+      console.log(array);
+        return array;
+      }
 
     return publicAPI;
 })();
