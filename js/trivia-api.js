53cdf71527b1b71294e271e9f2609bc22126877d
KikoLabiano <flabiano@hotmail.com>
2018-11-04 22:09:24 +0100

    Cambios

--- js/trivia-api.js
+++ js/trivia-api.js
@@ -1,37 +1,54 @@
-var triviaAPI= (function(){
+var triviaAPI = (function () {
 
 
 
     var publicAPI = {
-        getQuestion: (difficulty,category,type)=>{
-            return fetch('https://opentdb.com/api.php?amount=1') 
-            .then((response)=>{              
-                return response.json(); 
-            });
+        getQuestion: (difficulty, category, type) => {
+            //Compose API call
+            if (difficulty !== "any") {
+                difficulty = "&difficulty=" + difficulty;
+            } else {
+                difficulty = "";
+            }
+            if (category !== "any") {
+                category = "&category=" + category;
+            } else {
+                category = "";
+            }
+            if (type !== "any") {
+                type = "&type=" + type;
+            } else {
+                type = "";
+            }
+            return fetch('https://opentdb.com/api.php?amount=1' + difficulty + category + type)
+                .then((response) => {
+                    return response.json();
+                });
         },
-        shuffleAnswers: (answers)=>{
+        shuffleAnswers: (answers) => {
             return shuffle(answers);
         }
     };
 
     function shuffle(array) {
-        let currentIndex = array.length, temporaryValue, randomIndex;
+        let currentIndex = array.length,
+            temporaryValue, randomIndex;
         console.log(array);
         // While there remain elements to shuffle...
         while (0 !== currentIndex) {
-      
-          // Pick a remaining element...
-          randomIndex = Math.floor(Math.random() * currentIndex);
-          currentIndex -= 1;
-      
-          // And swap it with the current element.
-          temporaryValue = array[currentIndex];
-          array[currentIndex] = array[randomIndex];
-          array[randomIndex] = temporaryValue;
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
         }
-      console.log(array);
+        console.log(array);
         return array;
-      }
+    }
 
     return publicAPI;
 })();
